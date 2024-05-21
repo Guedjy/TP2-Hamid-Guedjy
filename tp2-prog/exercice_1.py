fb3dbdf4ca5416afb4b0e7dbd9e6a9b8636dfae2
Guedjy <e2363135@cmaisonneuve.qc.ca>
2024-05-20 23:24:24 -0400

    modification

--- tp2-prog/exercice_1.py
+++ tp2-prog/exercice_1.py
@@ -1,9 +1,7 @@
-
 import json
 import csv
 
-
-# Fonction pour écrire  nombres complexes dans fichier CSV
+# Fonction pour écrire des nombres complexes dans un fichier CSV
 def ecrire_fichier_csv(nombres_complexes, nom_fichier):
     with open(nom_fichier, 'w', newline='') as fichier:
         writer = csv.writer(fichier)
@@ -13,25 +11,21 @@ def ecrire_fichier_csv(nombres_complexes, nom_fichier):
         for nombre in nombres_complexes:
             writer.writerow([nombre.real, nombre.imag])
 
-
-# Fonction pour lire  fichier JSON et retourner nombres complexes
+# Fonction pour lire un fichier JSON et retourner des nombres complexes
 def lire_fichier_json(nom_fichier):
     with open(nom_fichier, 'r') as fichier:
         donnees = json.load(fichier)
-    nombres_complexes = [complex(reel, imaginaire) for reel, imaginaire in donnees]
+    nombres_complexes = [complex(donnee['reel'], donnee['imaginaire']) for donnee in donnees]
     return nombres_complexes
-
-
 
 # Nom des fichiers
 fichier_json = 'data.json'
 fichier_csv = 'nombres_complexes.csv'
 
-# Lire  fichier JSON
+# Lire le fichier JSON
 nombres_complexes = lire_fichier_json(fichier_json)
 
 # Écrire les nombres dans le fichier CSV
 ecrire_fichier_csv(nombres_complexes, fichier_csv)
 
-print("Les nombres complexes ont été sont dans '{}'.".format(fichier_csv))
-
+print("Les nombres complexes ont été écrits dans '{}'.".format(fichier_csv))
